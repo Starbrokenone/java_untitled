7e0087a38beb89542ee40565e7d61f24ca79260b
zhangliyun <1819269990@qq.com>
2018-11-16 22:24:52 +0800

    实验一 java实验基础

--- src/labreport/Test1_6.java
+++ src/labreport/Test1_6.java
@@ -8,7 +8,7 @@ package labreport;
  */
 public class Test1_6 {
     public static void main(String args[]){
-        int i,year=0;
+        int i;
         for (i = 1900;i <= 2100;i++){
             if (i % 4 ==0 && i % 100 !=0)
                 System.out.println(i);
