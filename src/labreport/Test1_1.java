393bfe129bdbf3c28704c34d0da552481722d375
zhangliyun <1819269990@qq.com>
2018-11-20 23:07:09 +0800

    实验三 Java面向对象程序设计

--- src/labreport/Test1_1.java
+++ src/labreport/Test1_1.java
@@ -7,7 +7,7 @@ public class Test1_1 {
     public static void main(String[] args) {
         int i = 0;
         int sum = 0;
-        while (i < 100) {
+        while (i <= 100) {
             sum = sum + i;
             i = i + 2;
 
