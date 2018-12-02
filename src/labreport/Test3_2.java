cec3a54456b69686bd090bd1b4a67de17debad29
zhangliyun <1819269990@qq.com>
2018-12-02 20:41:48 +0800

    实验三 java面向对象程序设计

--- src/labreport/Test3_2.java
+++ src/labreport/Test3_2.java
@@ -1,33 +1,36 @@
 package labreport;
 
+import java.util.Scanner;
 /**
- * 创建银行账号Account类，
- * 实现存款(balance)的存(deposit)、取(withdraw)和查询(getbalance)等功能
+ * 实现取款机功能，存款balance、存钱deposit、取withdraw、查询getbalan
+ *
  */
 public class Test3_2 {
-    public static void main(String args[]){
-        Account money = new Account(100.00);
-        money.deposit(100);
-        money.withdraw(20);
-        money.getbalance();
-    }
-}
-class Account{
-    double balance = 0.0f;
-    public Account(double _balance){
-        balance = _balance;
-        System.out.println("您的当前余额为"+balance);
-    }
-    public double deposit(double a){
-        this.balance += a;
-        System.out.println("存款后您的余额为"+balance);
-        return balance;
-    }
-    public void getbalance(){
-        System.out.println("您的当前余额为"+balance);
-    }
-    public void withdraw(double b){
-        this.balance -=b;
-        System.out.println("取款后您的余额为"+balance);
+    public static void main(String[] args) {
+        double balance = 0;
+        double deposit = 0;
+        double withdraw = 0;
+        for (;;) {
+            System.out.println("请输入数字：\t1.存钱 \t2.取钱\t3.查询余额\n");
+            Scanner input = new Scanner(System.in);
+            String num = input.next();
+            double s1 = 0, s2= 0 ;
+            if (num.equals("1")) {
+                System.out.println("输入存入金额：");
+                deposit = input.nextDouble();
+                System.out.println("存钱数目为:" + deposit);
+                s1 += deposit;
+            }
+            if (num.equals("2")) {
+                System.out.println("输入取钱金额：");
+                withdraw = input.nextDouble();
+                System.out.println("取走金额为：" + withdraw);
+                s2 += withdraw;
+            }
+            if (true | num.equals("3")) {
+                balance += s1 - s2;
+                System.out.println("账户余额为：" + balance);
+            }
+        }
     }
 }
